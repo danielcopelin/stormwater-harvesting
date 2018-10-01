709e20913a5a0d7a4ddc644f118fff3e61a9dfd7
Dan <danielcopelin@gmail.com>
2018-10-01 12:52:07 +1000

    Added basic stormwater harvesting functionality

--- app.py
+++ app.py
@@ -13,6 +13,8 @@ import stormwater_harvesting
 app = dash.Dash(__name__)
 server = app.server
 
+DF = stormwater_harvesting.parse_dnrm_data(r"notebooks\data\143028A.csv")
+
 CACHE_CONFIG = {
     'CACHE_TYPE': 'redis',
     'CACHE_REDIS_URL': os.environ.get('REDIS_URL', 'localhost:6379')
@@ -28,8 +30,84 @@ app.css.append_css({
 app.title = 'Stormwater Harvesting'
 
 app.layout = html.Div([
-
+                dcc.Graph(
+                    id='harvesting',
+                ),
+                dcc.Markdown('Demand (cumecs):'),
+                dcc.Input(
+                    id='demand',
+                    placeholder='Enter a value...',
+                    inputmode='numeric',
+                    # type='number',
+                    value=0.001
+                ),
+                dcc.Markdown('Max Tank Volume (m3):'),
+                dcc.Input(
+                    id='max-volume',
+                    placeholder='Enter a value...',
+                    inputmode='numeric',
+                    # type='number',
+                    value=100
+                ),
+                dcc.Markdown('Starting Tank Volume (m3):'),
+                dcc.Input(
+                    id='start-volume',
+                    placeholder='Enter a value...',
+                    inputmode='numeric',
+                    # type='number',
+                    value=50
+                ),   
+                dcc.Markdown('Pump Flow Rate (cumecs):'),
+                dcc.Input(
+                    id='pump-flow',
+                    placeholder='Enter a value...',
+                    inputmode='numeric',
+                    # type='number',
+                    value=0.05
+                ),                
 ])
+
+@app.callback(
+    dash.dependencies.Output('harvesting', 'figure'),
+    [dash.dependencies.Input('demand', 'value'),
+     dash.dependencies.Input('max-volume', 'value'),
+     dash.dependencies.Input('start-volume', 'value'),
+     dash.dependencies.Input('pump-flow', 'value'),
+    ],
+    )
+def update_chart(demand, max_volume, start_volume, pump_flow):
+    harvesting = stormwater_harvesting.harvesting_calcs(
+        DF, float(demand), float(max_volume), float(start_volume), float(pump_flow))
+
+    data = [
+        go.Scatter(
+            x=harvesting.index, 
+            y=harvesting.Tank_Volume, 
+            name='tank volume')
+    ]
+
+    return {
+        'data': data,
+        'layout': go.Layout(
+            xaxis=dict(
+                # range=[0,max_time/np.timedelta64(1, 'm')],
+                title='Date',
+                # domain=[0,1],
+                position=0
+            ),
+            yaxis=dict(
+                # range=[0,max_flow*1.1],
+                title='Tank Volume (m3)',
+                # domain=[0.0,1]
+            ),
+            # legend=go.Legend(
+            #     orientation='h'
+            #     # x=0.5, y=0.9
+            #     ),
+            # margin={'l':65, 'b': 40, 't': 10, 'r': 0},
+            # height=600
+        )
+        }
 
 if __name__ == '__main__':
     app.run_server(debug=True)
--- stormwater_harvesting.py
+++ stormwater_harvesting.py
@@ -1,2 +1,60 @@
 import pandas as pd
 import numpy as numpy
+
+def parse_dnrm_data(source):
+    """Parses DNRM format rainfall and streamflow data.
+    
+    Arguments:
+        source {csv file} -- A csv file from DNRM's Water Monitoring 
+        Portal, with Rainfall (mm) and Discharge (cumecs) columns.
+    
+    Returns:
+        DataFrame -- A DataFrame with a Datetime index, and colums for
+        Rainfall, Discharge, Volume and Timestep.
+    """
+
+    df = pd.read_csv(source, usecols=[0, 1, 3], skiprows=3, 
+                 skipfooter=3, index_col=0, parse_dates=True,
+                 dayfirst=True, engine='python')
+
+    df.columns = ['Rainfall', 'Discharge']
+    df.Discharge = df.Discharge.fillna(0)
+    df['Timestep'] = (df.index.to_series().diff() / pd.Timedelta(1, 's'))
+    df['Volume'] = (df.Timestep * df.Discharge.rolling(2).mean()).fillna(0)
+    
+    return df
+
+def harvesting_calcs(data, demand, max_volume, start_volume, pump_flow):
+    """[summary]
+    
+    Arguments:
+        data {[type]} -- [description]
+    """
+
+    def _harvest_flow(row, pump_flow):
+        harvest_flow = min(row.Discharge, pump_flow)
+        return harvest_flow
+
+    df = data.copy()
+
+    df['Harvest_Flow'] = df.apply(_harvest_flow, args=[pump_flow], axis=1)
+    df['Harvest_Volume'] = (df.Timestep * df.Harvest_Flow.rolling(2).mean()).fillna(0)
+
+    df['Demand'] = demand * ~df.Rainfall.astype(bool) # only irrigate if not raining
+    df['Demand_Volume'] = (df.Timestep * df.Demand.rolling(2).mean()).fillna(0)    
+
+    tank_volume = [start_volume]
+    harvest_actual = [0]
+
+    for i in range(1, len(df)):
+        prev_volume = tank_volume[i-1]
+        new_volume = prev_volume + df.Harvest_Volume.values[i] - df.Demand_Volume.values[i]
+        if new_volume < 0:
+            new_volume = 0
+        elif new_volume > max_volume:
+            new_volume = max_volume
+        tank_volume.append(new_volume)
+        
+    df['Tank_Volume'] = tank_volume
+
+    return df
